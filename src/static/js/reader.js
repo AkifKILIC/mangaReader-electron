e3895704473672d4401f3b816b19932ee1a05884
AkifKILIC <45569484+AkifKILIC@users.noreply.github.com>
2021-04-30 21:55:31 +0300

    Fixed Minor Errors and Added Puppeteer

--- src/static/js/reader.js
+++ src/static/js/reader.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 var chapterPageLeft = document.getElementById("left");
 var chapterPageRight = document.getElementById("right");
 
@@ -8,5 +9,12 @@ function chapterRight(){
     console.log("right");
 };
 function loadChapter(){
-
+    var lol = fetch("https://manganelo.com/chapter/hyer5231574354229/chapter_1");
+    
+    alert(lol.toString());
 };
+async function scrapeProuct(url){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+}
