dc79df5055648981783d25f6ba7304f886298374
AkifKILIC <45569484+AkifKILIC@users.noreply.github.com>
2021-05-08 01:27:11 +0300

    Finished Reader page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+require('electron-referer')('https://manganelo.com/');
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -36,7 +37,10 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+/*app.on('ready', () =>{
+  win = new BrowserWindow();
+})*/
+app.on('ready',createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
--- src/static/js/reader.js
+++ src/static/js/reader.js
@@ -6,15 +6,58 @@ const { WriteStream } = require('tty');
 
 var chapterPageLeft = document.getElementById("left");
 var chapterPageRight = document.getElementById("right");
-function chapterLeft(){
-    console.log("left");
-};
-function chapterRight(){
-    console.log("right");
-};
+var _url = 'https://manganelo.com/chapter/hyer5231574354229/chapter_1';
+var _imgUrl = '';
+var _refUrl = _url.replace(_url.split('/')[5], '');
 var startUp = '<div class="col nopad page-left" id="left" onclick="chapterLeft()"></div><div class="col nopad page-right" id="right" onclick="chapterRight()"></div>';
 var newImages = '';
 var elText = '';
+
+function chapterLeft(){
+    console.log("left");
+};
+
+function chapterRight(){
+    console.log("right");
+};
+async function lod(){
+    var outside;
+    let promise = fetch('https://s7.mkklcdnv6tempv3.com/mangakakalot/r1/read_hajime_no_ippo_manga/chapter_1/1.jpg',
+    {
+        method: 'GET',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.125',
+            'referer' : 'https://mangakakalot.com/'
+        },
+        referer: 'https://mangakakalot.com/'
+    });
+    console.log(promise);
+}
+async function loadChapter(url){
+    _url = url;
+    var el = document.createElement('html');
+    let response =  await fetch(url,{
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.125',
+            'Referer' : _refUrl
+        }
+    });
+    let responseText = await getTextFromStream(response.body);
+    el.innerHTML = responseText;
+    var el2 = el.getElementsByClassName('container-chapter-reader');
+    if(el2.length == 1){
+        var el3 = el2.item(0).getElementsByTagName('img');
+        newImages = startUp;
+        options2.headers.Referer = url.replace(_url.split('/')[5], '');
+        for (i = (el3.length - 1); i >= 0 ; i--) {
+            newImages = newImages + '<img src="'+ el3.item(i).getAttribute('src') +'" id="img'+ i +'" style="width: auto;">';
+        }
+        document.getElementById('con').innerHTML = newImages;
+    }else{
+        console.error('el2 has no element!!!');
+    }
+}
+
 async function getTextFromStream(readableStream) {
     let reader = readableStream.getReader();
     let utf8Decoder = new TextDecoder();
@@ -29,63 +72,6 @@ async function getTextFromStream(readableStream) {
     
     return resultStr;
 }
-async function loadChapter(url){
-    options.url = url;
-    var el = document.createElement('html');
-    let response =  await fetch(url,{
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.125',
-            'Referer' : _refUrl
-        }
-    });
-    let responseText = await getTextFromStream(response.body);
-    el.innerHTML = responseText;
-    fs.readFile('src/static/temp/temp.txt', 'utf-8');
-    el.innerHTML = elText.toString(6);
-    var el2 = el.getElementsByClassName('container-chapter-reader');
-    if(el2.length == 1){
-        var el3 = el2.item(0).getElementsByTagName('img');
-        newImages = startUp;
-        options2.headers.Referer = url.replace(_url.split('/')[5], '');
-        for (i = 0; i < el3.length; i++) {
-            options2.url = el3.item(i).getAttribute('src');
-            request(options2).pipe(fs.createWriteStream('src/static/temp/imgtemp/temp'+ i +'.txt'));
-            newImages = newImages + '<img src="'+ 'static/temp/imgtemp/temp'+ i +'.txt' +'" id="img'+ i +'" style="width: auto;">';
-        }
-        document.getElementById('con').innerHTML = newImages;
-        /*for(i = 0; i < el3.length; i++){
-            fs.unlink('src/static/temp/imgtemp/temp'+ i +'.txt', (err) => {
-                if(err){
-                    alert(err);
-                    throw err;
-                }
-            });
-        }
-    }else{
-        console.error('el2 has no element!!!');
-    }
-    fs.unlink('src/static/temp/temp.txt', (err) => {
-        if(err){
-            alert(err);
-            throw err;
-        }
-    })*/
-}
-function rQuest(option,path){
-    request(option).pipe(fs.createWriteStream(path));
-}
-function readFile(path){
-
-}
-var _url = 'https://manganelo.com/chapter/hyer5231574354229/chapter_1';
-var _imgUrl = '';
-var _refUrl = _url.replace(_url.split('/')[5], '');
-const options = {
-    url: _url.toString(),
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.125'
-    }
-};
 /*
 const filter = {
     urls: ['https://manganelo.com/chapter/hyer5231574354229/chapter_1']
