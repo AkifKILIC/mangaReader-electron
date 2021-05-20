c18f8e0cc4cf1b2bd1a80462c92137305a259afd
AkifKILIC <45569484+AkifKILIC@users.noreply.github.com>
2021-05-20 21:47:20 +0300

    Corrections

--- src/static/js/reader.js
+++ src/static/js/reader.js
@@ -1,4 +1,3 @@
-
 var chapterPageLeft = document.getElementById("left");
 var chapterPageRight = document.getElementById("right");
 var _url = 'https://manganelo.com/chapter/hyer5231574354229/chapter_1';
@@ -8,47 +7,45 @@ var startUp = '<div class="col nopad page-left" id="left" onclick="chapterLeft()
 var newImages = '';
 var elText = '';
 
-function chapterLeft(){
+function chapterLeft() {
     console.log("left");
 };
 
-function chapterRight(){
+function chapterRight() {
     console.log("right");
 };
-async function lod(){
+async function lod() {
     var outside;
-    let promise = fetch('https://s7.mkklcdnv6tempv3.com/mangakakalot/r1/read_hajime_no_ippo_manga/chapter_1/1.jpg',
-    {
+    let promise = fetch('https://s7.mkklcdnv6tempv3.com/mangakakalot/r1/read_hajime_no_ippo_manga/chapter_1/1.jpg', {
         method: 'GET',
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.125',
-            'referer' : 'https://mangakakalot.com/'
+            'referer': 'https://mangakakalot.com/'
         },
         referer: 'https://mangakakalot.com/'
     });
     console.log(promise);
 }
-async function loadChapter(url){
+async function loadChapter(url) {
     _url = url;
     var el = document.createElement('html');
-    let response =  await fetch(url,{
+    let response = await fetch(url, {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.125',
-            'Referer' : _refUrl
+            'Referer': _refUrl
         }
     });
     let responseText = await getTextFromStream(response.body);
     el.innerHTML = responseText;
     var el2 = el.getElementsByClassName('container-chapter-reader');
-    if(el2.length == 1){
+    if (el2.length == 1) {
         var el3 = el2.item(0).getElementsByTagName('img');
         newImages = startUp;
-        for (i = (el3.length - 1); i >= 0 ; i--) {
-            newImages = newImages + '<img src="'+ el3.item(i).getAttribute('src') +'" id="img'+ i +'" style="width: auto;">';
+        for (i = (el3.length - 1); i >= 0; i--) {
+            newImages = newImages + '<img src="' + el3.item(i).getAttribute('src') + '" id="img' + i + '" style="width: auto;">';
         }
         document.getElementById('con').innerHTML = newImages;
-    }else{
+    } else {
         console.error('el2 has no element!!!');
     }
 }
-
